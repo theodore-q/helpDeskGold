7e9d85b2f2d44cf1ad9888d76b1c47dfc25eba42
theodore-q <theo.q.er@gmail.com>
2016-11-28 16:46:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ var cardsToTemplates = function (cards) {
     return templates;
 };
 
-loadJSON('/dataSource.json', function (cards) {
+loadJSON('dataSource.json', function (cards) {
     var templates = cardsToTemplates(cards);
     
     for (var i = 0; i < templates.length; i++) {
